9ffadccdf6689ff4d035e04fcd95f057d656962f
Yazz <itsjustmaitai@gmail.com>
2025-03-07 11:24:18 +0300

    compete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser="John";
+let welcomeMessage= "Welcome to Flatbook, John!";
+let excitedWelcomeMessage="WELCOME TO FLATBOOK, JOHN !";
+let shortGreeting="Welcome, J!";
